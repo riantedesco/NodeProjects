3fb580e9512cdf7712e663592e9137bde39960aa
Rian Tedesco <rct.tedesco@gmail.com>
2022-03-13 15:30:17 -0300

    Commit de 13/03/2022-15h30m16s

---
+++ apicolaboradores/server.js
@@ -0,0 +1,25 @@
+console.log("Server rodando...");
+
+// usar o express
+const express = require('express');
+const app = express();
+app.use(express.json()); // para tratar json
+
+// conexão com mongoBD
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+const uri = "mongodb://admin:admin@localhost:27018/teste?authSource=teste ";
+MongoClient.connect(uri, (err, client) => {
+    if (err)
+        return console.log(err);
+    db = client.db('teste');
+    app.listen(3000, function () { // subir serviço da api na porta 3000
+        console.log('API rodando na porta 3000');
+        console.log('Testar por http://localhost:3000');
+    });
+});
+
+// prerarar para responder ao GET
+app.get('/', (req, res) => {
+    res.send('Atendida a requisição GET!!');
+   }); 
